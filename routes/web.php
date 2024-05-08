672dc57579aa555625f6f0087f6f457514afe08e
alessandrodamato <alessandro042004@gmail.com>
2024-05-08 15:00:06 +0200

    [add] route to home page with dynamic data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = ['title' => 'Home Page'];
+    return view('home', $data);
+})->name('home');
